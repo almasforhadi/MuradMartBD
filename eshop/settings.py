c632d4fcc6119936f2851ec7928ae0685c2c21d8
Almas-Forhadi <almasforhasi1999@gmail.com>
2026-03-23 02:37:31 +0600

    error fixed

--- eshop/settings.py
+++ eshop/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.sites',
+    # 'django.contrib.sites',
     'shop',
     'allauth',
     'allauth.account',
