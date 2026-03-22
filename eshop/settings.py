cc87c09769bc190bc2aa8b6b56f72cd1b0288901
Almas-Forhadi <almasforhasi1999@gmail.com>
2026-03-22 08:46:55 +0600

    second commit

--- eshop/settings.py
+++ eshop/settings.py
@@ -27,10 +27,12 @@ SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG')
 
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.onrender.com', 'muradmartBD.com', 'www.muradmartBD.com']
 
-# CSRF_TRUSTED_ORIGINS = ['https://muradmart.onrender.com']
-
+CSRF_TRUSTED_ORIGINS = [
+    "https://muradmartBD.com",
+    "https://www.muradmartBD.com"
+]
 
 # Application definition
 
@@ -101,16 +103,16 @@ WSGI_APPLICATION = 'eshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-#     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 
 # Password validation
