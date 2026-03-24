d1ed621d8628c231742e51503cf2eb1341f060ef
almas-forhadi <163671225+almasforhadi@users.noreply.github.com>
2026-03-24 11:00:26 +0600

    Comment out media file
    Comment out the media file setup for Cloudinary use.

--- eshop/urls.py
+++ eshop/urls.py
@@ -11,4 +11,4 @@ urlpatterns = [
 ]
 
 # media file setup
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) # Cloudinary use for media file
